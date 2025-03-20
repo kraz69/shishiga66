8ad43b2841408ced7f87047bf768a210656d44c1
kraz69 <glazov11kirill@gmail.com>
2025-03-20 21:56:24 +1000

    Update hqsrv66DNS.sh

--- hqsrv66DNS.sh
+++ hqsrv66DNS.sh
@@ -16,20 +16,24 @@ systemctl enable --now dnsmasq
 
 # Настройка файла /etc/dnsmasq.conf
 {
-    echo "no-resolv"
-    echo "domain=au-team.irpo"
-    echo "server=8.8.8.8"
-    echo "interface=ens18"
-    echo "address=/hq-rtr.au-team.irpo/192.168.1.1"
-    echo "ptr-record=1.1.168.192.in-addr.arpa,hq-rtr.au-team.irpo"
-    echo "cname=moodle.au-team.irpo,hq-rtr.au-team.irpo"
-    echo "cname=wiki.au-team.irpo,hq-rtr.au-team.irpo"
-    echo "address=/br-rtr.au-team.irpo/192.168.4.1"
-    echo "address=/hq-srv.au-team.irpo/192.168.1.2"
-    echo "ptr-record=2.1.168.192.in-addr.arpa,hq-srv.au-team.irpo"
-    echo "address=/hq-cli.au-team.irpo/192.168.2.10"
-    echo "ptr-record=2.2.168.192.in-addr.arpa,hq-cli.au-team.irpo"
-    echo "address=/br-srv.au-team.irpo/192.168.4.2"
+echo "no-resolv"
+echo "no-poll"
+echo "no-hosts"
+echo "listen-address=192.168.1.10"
+echo "server=77.88.8.8"
+echo "server=195.208.4.1"
+echo "server=195.208.5.1"
+echo "server=8.8.8.8"
+echo "cache-size=1000"
+echo "all-servers"
+echo "no-negcache"
+echo "host-record=hq-rtr.au-team.irpo,192.168.1.1"
+echo "host-record=hq-srv.au-team.irpo,192.168.1.10"
+echo "host-record=hq-cli.au-team.irpo,192.168.2.10"
+echo "address=/br-rtr.au-team.irpo/192.168.3.1"
+echo "address=/br-srv.au-team.irpo/192.168.3.10"
+echo "cname=moodle.au-team.irpo,hq-rtr.au-team.irpo"
+echo "cname=wiki.au-team.irpo,hq-rtr.au-team.irpo"
 } > /etc/dnsmasq.conf
 
 # Настройка файла /etc/hosts
