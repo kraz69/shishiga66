6d0945b986590f2de23eddda92b74f02dd10fecf
kraz69 <glazov11kirill@gmail.com>
2025-03-20 20:40:46 +1000

    Create os66.sh

---
+++ os66.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Обновление списка пакетов
+apt-get update
+
+# Установка FRR
+apt-get install -y frr
+
+# Включение OSPF в конфигурации FRR
+sed -i 's/^#ospfd=ospfd/ospfd=yes/' /etc/frr/daemons
+
+# Перезапуск службы FRR
+systemctl restart frr
+
+# Настройка OSPF через vtysh
+{
+    echo "conf t"
+    echo "router ospf"
+    echo "ospf router-id 1.1.1.1"
+    echo "network 10.10.0.0/30 area 0"
+    echo "network 192.168.1.0/26 area 0"
+    echo "network 192.168.2.0/28 area 0"
+    echo "exit"
+    echo "int gre1"
+    echo "ip ospf authentication message-digest"
+    echo "ip ospf message-digest-key 1 md5 P@ssw0rd"
+    echo "exit"
+    echo "do wr mem"
+} | vtysh
