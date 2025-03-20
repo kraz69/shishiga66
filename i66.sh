78eea1bc21ba431dbf1a2f8c186115ed0f6fd7ef
kraz69 <glazov11kirill@gmail.com>
2025-03-20 16:14:57 +1000

    Update i66.sh

--- i66.sh
+++ i66.sh
@@ -24,15 +24,16 @@ sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/net/sysctl.con
 # Установка iptables
 apt-get install -y iptables
 
-# Включение службы iptables
-systemctl enable iptables --now
-
 # Настройка NAT с помощью iptables
 iptables -t nat -A POSTROUTING -s 172.16.4.0/28 -o ens18 -j MASQUERADE 
 iptables -t nat -A POSTROUTING -s 172.16.5.0/28 -o ens18 -j MASQUERADE
 
 # Сохранение правил iptables
 iptables-save > /etc/sysconfig/iptables
+
+# Автозапуск и перезапуск
+systemctl enable iptables
+systemctl restart iptables
 
 ##
 exec bash
