39feaca4ab0da242be5e5337a7359c4624cf9128
kraz69 <glazov11kirill@gmail.com>
2025-03-20 17:12:31 +1000

    Update b_rt66.sh

--- b_rt66.sh
+++ b_rt66.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+hostnamectl set-hostname br-rtr.au-team.irpo
 
 mkdir -p /etc/net/ifaces/ens19
 mkdir -p /etc/net/ifaces/gre1
@@ -15,3 +16,5 @@ echo "10.10.0.2/30" > /etc/net/ifaces/gre1/ipv4address
 echo -e "TYPE=iptun \nTUNTYPE=gre \nTUNLOCAL=172.16.5.5 \nTUNREMOTE=172.16.4.4 \nTUNTTL=64 \nTUNOPTIONS='ttl 64' \nDISABLE=no " > /etc/net/ifaces/gre1/options
 
 echo -e "search au-team.irpo \nnameserver 192.168.1.10" > /etc/net/ifaces/gre1/resolv.conf
+
+exec bash
