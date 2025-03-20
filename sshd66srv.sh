364453b79b804aafcd3a55685598597f8b499cb6
kraz69 <glazov11kirill@gmail.com>
2025-03-20 19:41:46 +1000

    Update sshd66srv.sh

--- sshd66srv.sh
+++ sshd66srv.sh
@@ -2,22 +2,24 @@
 
 # Установка OpenSSH
 apt-get update
-apt-get install -y openssh-common
+apt-get install -y openssh-server
+
 # Настройка конфигурационного файла sshd_config
 {
     echo "Port 2024"
     echo "MaxAuthTries 2"
     echo "AllowUsers sshuser"
     echo "PermitRootLogin no"
+    echo "Banner /root/banner"
 } | sudo tee -a /etc/ssh/sshd_config > /dev/null
 
 # Создание баннера
 echo "Authorized access only" | sudo tee /root/banner > /dev/null
 echo "" | sudo tee -a /root/banner > /dev/null  # Добавление пустой строки
 
-# Установка настроек службы SSHD
-systemctl enable --now sshd
-systemctl restart sshd
+# Включение и перезапуск службы SSH
+systemctl enable --now ssh
+systemctl restart ssh
 
 # Вывод сообщения об успешном завершении
 echo "SSH настроен и служба перезапущена."
